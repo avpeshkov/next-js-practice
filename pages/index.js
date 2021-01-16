8a40e6378c6ad57caefdce108e981b620d91bec6
apeshkov <apeshkov@evelopers.com>
2021-01-16 21:38:34 +0300

    [ft/create-blog] add data fetching for server-side-rendering

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from "next/link";
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+
+export default function Home({ allPostsData }) {
     return (
         <Layout home>
             <Head>
@@ -11,6 +14,33 @@ export default function Home() {
             <section className={utilStyles.headingMd}>
                 <p>The World Is Going to Hell and I'm Driving the Bus</p>
             </section>
+            <Link href="/posts/first-post">
+                <a>My first post</a>
+            </Link>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {date}
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </Layout>
     )
+}
+
+
+export async function getServerSideProps(context) {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
 }
